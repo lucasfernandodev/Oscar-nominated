9f7ff7150c90f5888bf084b69f75e85e20ecd6a6
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-04-07 17:22:22 -0300

    add link to server

--- assets/js/loadingMovies.js
+++ assets/js/loadingMovies.js
@@ -1,5 +1,5 @@
 async function loadingMovies(skip = 0) {
-  const response = await fetch(`http://192.168.100.111:3333/movies?skip=${skip}`);
+  const response = await fetch(`https://oscar2022api.herokuapp.com/movies?skip=${skip}`);
   const result = response.status === 200 ? await response.json() : null
 
   if (result !== null) {
